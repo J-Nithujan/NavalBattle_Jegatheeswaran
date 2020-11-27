e6b4538c0ade4ff8dcb39ae82bf571be44ebd903
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2020-11-27 09:54:29 +0100

    Update main.c
    Menu et aide dans le main

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /****************************************
-*   Title   :   NavalBattle   *
-*   Author  :   Jegatheeswaran Nithujan      *
-*   Version :   [26-11-2020]            *
+*   Title   :   NavalBattle             *
+*   Author  :   Jegatheeswaran Nithujan *
+*   Version :   [27-11-2020] v0.1       *
 ****************************************/
 
 #include <stdio.h>
@@ -9,12 +9,13 @@
 #include <windows.h>
 
 /**Global variables**/
-char board[10][10];
+int gameBoard[10][10];
+int choice = 0;
 
 /**Fonctions**/
 //In-game help
-void help(){
-    printf("Aide de jeu\n===========\n\n"
+void showHelp() {
+    printf("Aide de jeu\n===========\n"
            "Le but du jeu est de couler tous les bateaux.\n"
            "Pour ce faire il faut tirer sur une des cases du tableau en indiquant une coordonnée.\n"
            "Répéter cette opération jusqu'à ce que tous les bateaux soient coulées\n\n"
@@ -22,29 +23,53 @@ void help(){
            "Ratée: ~\n"
            "Touchée: X\n"
            "Coulée: O\n===========\n"
-           "Quitter (0)\n");
+           "Retour (0)\n");
+    scanf("%d", choice);
     return;
 }
-/*
-int mainMenu(){
-    int choice = 0;
-    printf("Bataille Naval\n==============\n\n"
-           "Nouvelle partie (1)\n"
-           "Aide (2)\n===========\n"
-           "Quitter(0)\n");
-    scanf("%d", &choice);
-    return choice; //A TESTER
-}
- */
 
-int gameBoard(){
 
-    return board;
+void gameBoardMaker() {
+
+    for (int i = 0; i < 10; ++i) {
+
+    }
+
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            printf("%d", gameBoard[i][j]);
+        }
+        printf("\n");
+    }
+    return;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
 
+    printf("Bataille Navale\n==============\n"
+           "Nouvelle partie (1)\n"
+           "Aide (2)\n"
+           "Quitter(3)\n"
+           "==============\n");
+    scanf("%d", &choice);
+
+
+    switch (choice) {
+        case 1:;
+            printf("Work in progress\n");
+            break;
+        case 2:
+            showHelp();
+            break;
+        case 3:
+            system("exit");
+            break;
+        default:
+            printf("Error");
+            break;
+    }
 
 
     return 0;
