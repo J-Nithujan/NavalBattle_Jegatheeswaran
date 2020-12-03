36e3d7a546f72808cb249e2d8501563400e0c218
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2020-12-03 16:56:33 +0100

    Update main.c
    Ajout de la grille de base et de fonctions gérant les choix de l'user dans le menu principal et le menu d'aide (pas fini)

--- main.c
+++ main.c
@@ -1,76 +1,112 @@
-/****************************************
-*   Title   :   NavalBattle             *
-*   Author  :   Jegatheeswaran Nithujan *
-*   Version :   [27-11-2020] v0.1       *
-****************************************/
+//Oublie pas conventions et commms
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 
 /**Global variables**/
-int gameBoard[10][10];
-int choice = 0;
+char gameBoard[10][10];
+int horizontalHeader[21] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+char verticalHeader[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+int choice = 0; /*???*/
 
 /**Fonctions**/
-//In-game help
-void showHelp() {
-    printf("Aide de jeu\n===========\n"
-           "Le but du jeu est de couler tous les bateaux.\n"
-           "Pour ce faire il faut tirer sur une des cases du tableau en indiquant une coordonnée.\n"
-           "Répéter cette opération jusqu'à ce que tous les bateaux soient coulées\n\n"
-           "État des cases:\n"
-           "Ratée: ~\n"
-           "Touchée: X\n"
-           "Coulée: O\n===========\n"
-           "Retour (0)\n");
-    scanf("%d", choice);
-    return;
-}
 
-
-void gameBoardMaker() {
-
-    for (int i = 0; i < 10; ++i) {
-
-    }
-
-
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            printf("%d", gameBoard[i][j]);
-        }
-        printf("\n");
-    }
-    return;
-}
-
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-
-    printf("Bataille Navale\n==============\n"
+void mainMenu()
+{
+    printf("Bataille Navale\n===============\n"
            "Nouvelle partie (1)\n"
            "Aide (2)\n"
            "Quitter(3)\n"
            "==============\n");
-    scanf("%d", &choice);
+    return;
+}
+
+//In-game help
+void showHelp()
+{
+    printf("===========\nAide de jeu\n===========\n"
+           "Le but du jeu est de couler tous les bateaux.\n"
+           "Pour ce faire il faut tirer sur une des cases du tableau en indiquant une coordonnée.\n"
+           "Répétez cette opération jusqu'à ce que tous les bateaux soient coulées\n\n"
+           "État des cases:\n"
+           "Case inconnue: ?\n"
+           "Ratée: ~\n"
+           "Touchée: X\n"
+           "Coulée: O\n===========\n"
+           "Retour (0)\n"
+           "===========\n");
+    return;
+}
 
 
-    switch (choice) {
-        case 1:;
-            printf("Work in progress\n");
+void gameBoardPrint()
+{
+    printf("%4c", ' ');
+
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%6d", horizontalHeader[i]);
+    }
+
+    printf("\n%4c", ' ');
+
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%3c%3c", 'v', '-');
+    }
+
+    printf("%2c\n%4c\n", 'v', '>');
+
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%2c", verticalHeader[i]);
+        printf("%2c", '|');
+
+        for (int j = 0; j < 10; ++j)
+        {
+            gameBoard[i][j] = '?';
+            printf("%6c", gameBoard[i][j]);
+        }
+
+        printf("\n%4c\n", '>');
+    }
+
+    return;
+}
+
+void menuInput(int input)
+{
+    switch (input)
+    {
+        case 1:
+            gameBoardPrint();
             break;
+
         case 2:
             showHelp();
             break;
+
         case 3:
             system("exit");
             break;
-        default:
-            printf("Error");
-            break;
     }
 
+    return;
+}
+
+void helpInput(int position, int input)
+{
+
+}
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    mainMenu();
+    scanf("%d", &choice);
+    menuInput(choice);
 
     return 0;
 }
