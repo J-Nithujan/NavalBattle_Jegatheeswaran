a718013c5af45f1b3d07b2885ffc30e235e84bb5
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2020-12-04 09:36:32 +0100

    Update main.c
    update of the help and main menu

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//Oublie pas conventions et commms
+///Oublie pas différents entête et les commms//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,41 +6,48 @@
 
 /**Global variables**/
 char gameBoard[10][10];
-int horizontalHeader[21] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int horizontalHeader[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 char verticalHeader[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+char boatsPosition[10][10] = ;
+char fiveLengthBoat[5] = {};
+char fourLengthBoats[4][2] = {};
+char threeLengthBoats[3][2] = {};
+char twoLengthBoat[2] = {};
 int choice = 0; /*???*/
 
 /**Fonctions**/
 
-void mainMenu()
-{
-    printf("Bataille Navale\n===============\n"
-           "Nouvelle partie (1)\n"
-           "Aide (2)\n"
-           "Quitter(3)\n"
-           "==============\n");
-    return;
-}
-
 //In-game help
 void showHelp()
 {
-    printf("===========\nAide de jeu\n===========\n"
-           "Le but du jeu est de couler tous les bateaux.\n"
-           "Pour ce faire il faut tirer sur une des cases du tableau en indiquant une coordonnée.\n"
-           "Répétez cette opération jusqu'à ce que tous les bateaux soient coulées\n\n"
-           "État des cases:\n"
-           "Case inconnue: ?\n"
-           "Ratée: ~\n"
-           "Touchée: X\n"
-           "Coulée: O\n===========\n"
-           "Retour (0)\n"
-           "===========\n");
+    do
+        {
+
+        printf("===========\nAide de jeu\n===========\n"
+               "Le but du jeu est de couler tous les bateaux.\n"
+               "Pour ce faire il faut tirer sur une des cases du tableau en indiquant une coordonnée.\n"
+               "Répétez cette opération jusqu'à ce que tous les bateaux soient coulées\n\n"
+               "État des cases:\n"
+               "Case inconnue: ?\n"
+               "Ratée: ~\n"
+               "Touchée: X\n"
+               "Coulée: O\n===========\n"
+               "Retour (0)\n"
+               "===========\n");
+
+        scanf("%d", choice);
+
+        if (choice != 0)
+        {
+            printf("UTILISEZ LA TOUCHE 0 (ZÉRO) POUR REVENIR AU JEU\n\n");
+        }
+    } while (choice =! 0);
+
     return;
 }
 
 
-void gameBoardPrint()
+void game()
 {
     printf("%4c", ' ');
 
@@ -72,15 +79,34 @@ void gameBoardPrint()
         printf("\n%4c\n", '>');
     }
 
+    printf("========\nAide (2)\n========");
+
     return;
 }
 
-void menuInput(int input)
+/*void mainMenu()
 {
-    switch (input)
+    do {
+
+        printf("===============\nBataille Navale\n===============\n"
+               "Nouvelle partie (1)\n"
+               "Aide (2)\n"
+               "Quitter(3)\n"
+               "==============\n");
+        scanf("%d", &choice);
+
+        if (choice != 1 && choice != 2 && choice != 3 )
+        {
+            printf("VEUILLEZ FAIRE UN CHOIX AVEC LES TOUCHES 1, 2 OU 3 !!!\n\n");
+        }
+
+    }while (choice != 1 && choice != 2 && choice != 3 );
+
+    switch (choice)
     {
+
         case 1:
-            gameBoardPrint();
+            game();
             break;
 
         case 2:
@@ -90,23 +116,21 @@ void menuInput(int input)
         case 3:
             system("exit");
             break;
+
     }
 
     return;
-}
+}*/
 
-void helpInput(int position, int input)
-{
 
-}
+
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
 
-    mainMenu();
-    scanf("%d", &choice);
-    menuInput(choice);
+    game();
+
 
     return 0;
 }
